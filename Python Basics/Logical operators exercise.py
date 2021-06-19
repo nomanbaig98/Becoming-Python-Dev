fb9216d73e3e4316149a1ce02b8cd7dc9046741a
Muhammad Noman Baig <noman-baig-1998@outlook.com>
2021-06-20 02:18:15 +0500

    progress added

---
+++ Python Basics/Logical operators exercise.py
@@ -0,0 +1,11 @@
+is_magician = True
+is_expert = False
+
+if is_magician and is_expert:
+    print('you are a master magician')
+
+elif is_magician and not is_expert:
+    print('atleast you\'re getting there')
+
+elif not is_magician:
+    print('you need magic powers')
