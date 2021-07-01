693633f8b9b8a54b68186f8b1c1591f357060b2d
Muhammad Noman Baig <noman-baig-1998@outlook.com>
2021-07-01 23:42:21 +0500

    progress added

--- Web scraping/Project Hacker News.py
+++ Web scraping/Project Hacker News.py
@@ -7,18 +7,25 @@ from bs4 import BeautifulSoup
 res = requests.get("https://news.ycombinator.com/news")
 soup = BeautifulSoup(res.text, 'lxml')
 links = soup.select('.storylink')
-votes = soup.select('.score')
+subtext = soup.select('.subtext')
 
 
-def create_custom_hn(links, votes):
+def sort_stories_by_votes(hnlist):
+    return sorted(hnlist, key=lambda k: k['votes'], reverse=True)
+
+
+def create_custom_hn(links, subtext):
     hn = []
     for index, item in enumerate(links):
         title = item.getText()
+        # when the link is broken or there is no link present then we use None as a second parameter
         href = item.get('href', None)
-        points = votes[index].getText()
-        print(points)
-        hn.append({'title': title, 'link': href})
-    return hn
+        vote = subtext[index].select('.score')
+        if len(vote):
+            points = int(vote[0].getText().replace(' points', ''))
+            if points > 99:
+                hn.append({'title': title, 'link': href, 'votes': points})
+    return sort_stories_by_votes(hn)
 
 
-pprint.pprint(create_custom_hn(links, votes))
+pprint.pprint(create_custom_hn(links, subtext))
