72fdfd45dc6f15ed9e0ddb4a2334a83fa7b4c4d4
Muhammad Noman Baig <noman-baig-1998@outlook.com>
2021-09-13 14:50:38 +0500

    progress added

--- Python Basics/password checker lengthy.py
+++ Python Basics/password checker lengthy.py
@@ -3,25 +3,26 @@ def get_password():
     s = str(input('enter your password:'))
     if (len(s) >= 8):
         for i in s:
-  
+
             # counting lowercase alphabets 
             if (i.islower()):
-                l+=1            
-  
-            # counting uppercase alphabets
+                l += 1
+
+                # counting uppercase alphabets
             if (i.isupper()):
-                u+=1            
-  
-            # counting digits
+                u += 1
+
+                # counting digits
             if (i.isdigit()):
-                d+=1            
-  
-            # counting the mentioned special characters
-            if(i=='#'or i=='$' or i=='%' or i=='&'):
-                p+=1           
-    if (l>=1 and u>=2 and p>=1 and d>=1 and l+p+u+d==len(s)):
+                d += 1
+
+                # counting the mentioned special characters
+            if (i == '#' or i == '$' or i == '%' or i == '&'):
+                p += 1
+    if (l >= 1 and u >= 2 and p >= 1 and d >= 1 and l + p + u + d == len(s)):
         print("Valid Password")
     else:
         print("Invalid Password")
-        
+
+
 get_password()
