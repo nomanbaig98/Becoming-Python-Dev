021ee2cf9932ff71b190b5f3ed8a94db989c2b2c
Noman Baig <noman-baig-1998@outlook.com>
2021-12-11 19:41:41 +0500

    progress added

--- check.py
+++ check.py
@@ -1,12 +1,12 @@
 def fibonacci(n):
-    a=1
-    b=1
+    a = 1
+    b = 1
     for i in range(n):
         yield a
-        a,b = b,a+b
+        a, b = b, a + b
+
 
 for x in fibonacci(10):
     print(x)
 
-
-#teaching ameer hussain
+# teaching ameer hussain
